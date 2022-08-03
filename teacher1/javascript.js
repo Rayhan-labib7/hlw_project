998b83f47035293500712875c83ec18a663e81c9
Rayhan-labib7 <rayhanlabib7@gmail.com>
2022-08-03 21:18:35 +0600

    teacher 1

---
+++ teacher1/javascript.js
@@ -0,0 +1,14 @@
+const tec="TEACHER";
+ReactDOM.render(
+    <div className="bg">
+        <div className="bg1">
+            <div className="t1">
+            <p>{tec}
+            </div>
+            <img src="teacher.png"></img>
+            </p>
+        </div>
+    </div>
+    ,
+    document.getElementById("root")
+);
